3392e6260d99331c0f3c46acc00f885977abbf91
sahilk335 <sahilk335@gmail.com>
2019-08-31 18:02:18 +0530

    ViewModel implemented

--- app/src/main/java/com/example/mvvmsampleapp/MainActivity.java
+++ app/src/main/java/com/example/mvvmsampleapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.mvvmsampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ask Android system for new View Model (don't just create yourself)
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        //Any time a live Data is changed this method will get called
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "LiveData Changed !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
