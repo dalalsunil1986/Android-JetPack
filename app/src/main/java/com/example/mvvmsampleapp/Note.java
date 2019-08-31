9e5d90f04de16bace16272b3ddbf94d7e11b020e
sahilk335 <sahilk335@gmail.com>
2019-08-31 15:55:52 +0530

    Created Room Database
    1. Created Entity
    2. Created DAO interface
    3. Created Singleton Database class

---
+++ app/src/main/java/com/example/mvvmsampleapp/Note.java
@@ -0,0 +1,39 @@
+package com.example.mvvmsampleapp;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String desc;
+    private int priority;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+}
